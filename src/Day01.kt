3ddfaf74933f88981b7b1728f60e1f3b08b80643
Adam Bennett <abennett.kva@gmail.com>
2022-12-01 07:22:23 -0500

    Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,39 @@
+import java.lang.Integer.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var currentMax = 0
+        var localSum = 0
+
+        for (value in input) {
+            if (value.isEmpty()) {
+                currentMax = max(currentMax, localSum)
+                localSum = 0
+            } else {
+                localSum += value.toInt()
+            }
+        }
+
+        return currentMax
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Int = buildList {
+        var localSum = 0
 
-    // test if implementation meets criteria from the description, like:
+        for (value in input) {
+            if (value.isEmpty()) {
+                add(localSum)
+                localSum = 0
+            } else {
+                localSum += value.toInt()
+            }
+        }
+    }.sorted()
+        .takeLast(3)
+        .sum()
+
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
